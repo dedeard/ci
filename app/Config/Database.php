5f5a9639199270551d9d6db6b4dd03e7ad4a3238
Dede ard <me@dedeard.my.id>
2025-01-30 09:42:09 +0800

    update database configuration with new hostname, username, password, and database name

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'hostname'     => '127.0.0.1',
+        'username'     => 'simrs',
+        'password'     => 'simrs',
+        'database'     => 'simrs',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
