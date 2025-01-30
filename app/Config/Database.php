ed3b27248ef9f3f3e1ed1fb40c81db508f5990c0
Dede ard <me@dedeard.my.id>
2025-01-30 11:23:54 +0800

    update database configuration in Database.php and docker-compose.yml for new MySQL credentials and port

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => '127.0.0.1',
-        'username'     => 'simrs',
-        'password'     => 'simrs',
-        'database'     => 'simrs',
+        'hostname'     => 'df518.h.filess.io',
+        'username'     => 'simrs_fatpathcut',
+        'password'     => '2697ec9823eecd494ecff0aa8496e71802981a1c',
+        'database'     => 'simrs_fatpathcut',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
@@ -41,7 +41,7 @@ class Database extends Config
         'compress'     => false,
         'strictOn'     => false,
         'failover'     => [],
-        'port'         => 3300,
+        'port'         => 3307,
         'numberNative' => false,
         'foundRows'    => false,
         'dateFormat'   => [
