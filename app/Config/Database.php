9e1b52b30f0536b5e8088b624d04a8495df9e745
Dede ard <me@dedeard.my.id>
2025-01-30 10:59:38 +0800

    update database hostname in Database.php configuration to mysql

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,7 +26,7 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
+        'hostname'     => 'mysql',
         'username'     => 'simrs',
         'password'     => 'simrs',
         'database'     => 'simrs',
